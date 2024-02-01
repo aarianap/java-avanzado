2ae3ae17b96fc6e8b9191f689cbc98399ae386a9
aarianap <aalvaropc2@gmail.com>
2024-02-01 17:47:52 -0500

    haciendo la introduccion de las matrices

---
+++ Matrices/src/Introduccion.java
@@ -0,0 +1,19 @@
+//Matrices
+/* Que son las matrices?
+ * Son arreglos bidimensionales, donde cada uno de sus elementos es a su vez un
+ * arreglo en su segunda dimension.
+ * Esto permite que no todos los elementos tengan el mismo tamaño.
+ * De esta forma es posible crear matrices recursivas y multi-dimensionales
+ * (un aarreglo compuesta de otros arreglos)
+ */
+//Representacion
+/*
+ * 0    1   2   3   4   5   6  ->Total de columnas length = 6
+ * 1    20  22  24  26  28  30
+ * 2    31  33  35  37  39  40  ->Numero 40 se encuentra en la posicion[2][6]
+ * 3    50  60  70  80  90  100
+ * ⬆->total de filas, length = 4
+ */
+
+public class Introduccion {
+}
