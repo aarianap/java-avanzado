e9d01d943ce619e3f1f26faffd3c047ac5328ccd
aarianap <aalvaropc2@gmail.com>
2024-02-03 19:40:21 -0500

    haciendo Ejemplo de matrices con for

---
+++ Matrices/src/EjemploMatricesStringFor.java
@@ -0,0 +1,17 @@
+public class EjemploMatricesStringFor {
+    public static void main(String[] args) {
+        String[][] nombres = new String[3][2];
+        nombres[0][0] = "Pepe";
+        nombres[0][1] = "Pepa";
+        nombres[1][0] = "Jose";
+        nombres[1][1] = "Josefa";
+        nombres[2][0] = "Paco";
+        nombres[2][1] = "Pancha";
+
+        for(int i = 0; i < nombres.length; i++)
+            for(int j = 0 ; j < nombres[i].length; j++){
+                System.out.println();
+            }
+        }
+    }
+}
