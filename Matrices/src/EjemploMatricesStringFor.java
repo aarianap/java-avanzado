7cc494594f449f2021ba6e7ad65f9d1ee9c8709a
ariana <aalvaropc2@gmail.com>
2024-02-04 18:28:42 -0500

    creando ejemplo de matrices con for

--- Matrices/src/EjemploMatricesStringFor.java
+++ Matrices/src/EjemploMatricesStringFor.java
@@ -1,4 +1,4 @@
-public class EjemploMatricesStringFor {
+public class EjemploMatricesStringFor{
     public static void main(String[] args) {
         String[][] nombres = new String[3][2];
         nombres[0][0] = "Pepe";
@@ -7,11 +7,24 @@ public class EjemploMatricesStringFor {
         nombres[1][1] = "Josefa";
         nombres[2][0] = "Paco";
         nombres[2][1] = "Pancha";
-
-        for(int i = 0; i < nombres.length; i++)
-            for(int j = 0 ; j < nombres[i].length; j++){
-                System.out.println();
+        //para obtener la cantidad filas: nombres.length
+        for (int i = 0; i < nombres.length;i++ ){
+            //para obtener la cantidad de columnas:
+            for (int j = 0; j < nombres[0].length; j++){
+                System.out.println(nombres[i][j] + "\t");
             }
+            System.out.println();
         }
+
+        System.out.println("== Iterando con foreach ==");
+
+        for(String[] fila : nombres ){
+            for (String nombre: fila){
+                System.out.println(nombre + "\t");
+            }
+            System.out.println();
+        }
+
     }
+}
 
