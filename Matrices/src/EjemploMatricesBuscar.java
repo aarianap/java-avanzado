08f419f933c10a10682bdd4386ec3c8b049bb03a
ariana <aalvaropc2@gmail.com>
2024-02-06 16:17:30 -0500

    Ejercicio suma de matrices

---
+++ Matrices/src/EjemploMatricesBuscar.java
@@ -0,0 +1,16 @@
+public class EjemploMatricesBuscar {
+    public static void main(String[] args) {
+        int[][] matrizDeEnteros = {
+                {35, 90, 3, 1978},
+                {15, 2020, 10, 5},
+                {677, 127, 32767,1999}
+        };
+
+        int elementoBuscar = 15;
+        boolean encontrado = false;
+
+        for(int i = 0; i < matrizDeEnteros.length;){
+
+        }
+    }
+}
