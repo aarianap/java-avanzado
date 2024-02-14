1064346f48609a2ccb90d221cacdb1887548f368
aarianap <aalvaropc2@gmail.com>
2024-02-13 22:28:19 -0500

    Utilizando metodos

--- ClasesyObjetos/src/Auto/Automovil.java
+++ ClasesyObjetos/src/Auto/Automovil.java
@@ -7,5 +7,16 @@ public class Automovil {
     String color;
     double cilindrada;
 
+    //Añadiendo metodos
+    //modificador de acceso //que devuelve tambien puede devolver nada //el nombre del metodo //poner el parametro puede estar vacio
+    public void detalle(){
+        //imprimiendo valores de auto
+        //this se refiere a si mismo
+        System.out.println("auto.fabricante = " +  this.fabricante);
+        System.out.println("auto.modelo = " + this.modelo );
+        System.out.println("auto.color = " + this.color);
+        System.out.println("auto.cilindrada = " + this.cilindrada);
+    }
+
 
 }
